3bd8308d9a0fcb08bcbd599e16efc4f2646995d9
Luis Hernandez <lhdez@iuma.ulpgc.es>
2022-03-19 11:22:18 +0000

    ajustando tests de app

--- app/src/test/java/es/ulpgc/eite/studentgrade/StudentGradeTests.java
+++ app/src/test/java/es/ulpgc/eite/studentgrade/StudentGradeTests.java
@@ -13,39 +13,50 @@ public class StudentGradeTests {
 
   StudentGradeSteps steps = new StudentGradeSteps();
 
-  @Test
-  public void Scenario1A() {
-    steps.iAmOnScreen("student");
-    steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
-  }
-
-  // -----
 
   @Test
-  public void Scenario2A() {
+  public void Scenario012A() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
+
+    // When
     steps.iAmOnScreenAndPressButton("student", "outstanding");
+
+    // Then
     steps.iAmOnScreen("grade");
     steps.iAmOnScreenAndSeeValueOnView("grade", "9", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "10", "higher");
   }
 
   @Test
-  public void Scenario3A() {
+  public void Scenario023A() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
+
+    // When
     steps.iAmOnScreenAndPressButton("student", "mention");
+
+    // Then
     steps.iAmOnScreen("grade");
     steps.iAmOnScreenAndSeeValueOnView("grade", "7", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "8", "higher");
   }
 
   @Test
-  public void Scenario4A() {
+  public void Scenario034A() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
+
+    // When
     steps.iAmOnScreenAndPressButton("student", "pass");
+
+    // Then
     steps.iAmOnScreen("grade");
     steps.iAmOnScreenAndSeeValueOnView("grade", "5", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "6", "higher");
@@ -54,27 +65,39 @@ public class StudentGradeTests {
   // -----
 
   @Test
-  public void Scenario21A() {
+  public void Scenario0421A() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
     steps.iAmOnScreenAndPressButton("student", "outstanding");
     steps.iAmOnScreen("grade");
     steps.iAmOnScreenAndSeeValueOnView("grade", "9", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "10", "higher");
+
+    // When
     steps.iAmOnScreenAndPressButton("grade", "lower");
+
+    // Then
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "9", "grade");
   }
 
   @Test
-  public void Scenario22A() {
+  public void Scenario0522A() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
     steps.iAmOnScreenAndPressButton("student", "outstanding");
     steps.iAmOnScreen("grade");
     steps.iAmOnScreenAndSeeValueOnView("grade", "9", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "10", "higher");
+
+    // When
     steps.iAmOnScreenAndPressButton("grade", "higher");
+
+    // Then
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "10", "grade");
   }
@@ -83,27 +106,39 @@ public class StudentGradeTests {
 
 
   @Test
-  public void Scenario31A() {
+  public void Scenario0631A() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
     steps.iAmOnScreenAndPressButton("student", "mention");
     steps.iAmOnScreen("grade");
     steps.iAmOnScreenAndSeeValueOnView("grade", "7", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "8", "higher");
+
+    // When
     steps.iAmOnScreenAndPressButton("grade", "higher");
+
+    // Then
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "8", "grade");
   }
 
   @Test
-  public void Scenario32A() {
+  public void Scenario0732A() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
     steps.iAmOnScreenAndPressButton("student", "mention");
     steps.iAmOnScreen("grade");
     steps.iAmOnScreenAndSeeValueOnView("grade", "7", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "8", "higher");
+
+    // When
     steps.iAmOnScreenAndPressButton("grade", "lower");
+
+    // Then
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "7", "grade");
   }
@@ -111,75 +146,232 @@ public class StudentGradeTests {
   // -----
 
   @Test
-  public void Scenario41A() {
+  public void Scenario0841A() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
     steps.iAmOnScreenAndPressButton("student", "pass");
     steps.iAmOnScreen("grade");
     steps.iAmOnScreenAndSeeValueOnView("grade", "5", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "6", "higher");
+
+    // When
     steps.iAmOnScreenAndPressButton("grade", "higher");
+
+    // Then
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "6", "grade");
   }
 
 
   @Test
-  public void Scenario42A() {
+  public void Scenario0942A() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
     steps.iAmOnScreenAndPressButton("student", "pass");
     steps.iAmOnScreen("grade");
     steps.iAmOnScreenAndSeeValueOnView("grade", "5", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "6", "higher");
+
+    // When
     steps.iAmOnScreenAndPressButton("grade", "lower");
+
+    // Then
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "5", "grade");
   }
 
-  // -----
 
-  @Test
-  public void Scenario1B() {
-    steps.iAmOnScreen("student");
-    steps.iRotateScreen("student");
-    steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
-  }
 
   // -----
 
   @Test
-  public void Scenario2B() {
+  public void Scenario102B() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
     steps.iAmOnScreenAndPressButton("student", "outstanding");
     steps.iAmOnScreen("grade");
-    steps.iRotateScreen("student");
+
+    // When
+    steps.iRotateScreen("grade");
+
+    // Then
     steps.iAmOnScreenAndSeeValueOnView("grade", "9", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "10", "higher");
   }
 
   @Test
-  public void Scenario3B() {
+  public void Scenario113B() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
     steps.iAmOnScreenAndPressButton("student", "mention");
     steps.iAmOnScreen("grade");
-    steps.iRotateScreen("student");
+
+    // When
+    steps.iRotateScreen("grade");
+
+    // Then
     steps.iAmOnScreenAndSeeValueOnView("grade", "7", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "8", "higher");
   }
 
   @Test
-  public void Scenario4B() {
+  public void Scenario124B() {
+
+    // Given
     steps.iAmOnScreen("student");
     steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
     steps.iAmOnScreenAndPressButton("student", "pass");
     steps.iAmOnScreen("grade");
-    steps.iRotateScreen("student");
+
+    // When
+    steps.iRotateScreen("grade");
+
+    // Then
     steps.iAmOnScreenAndSeeValueOnView("grade", "5", "lower");
     steps.iAmOnScreenAndSeeValueOnView("grade", "6", "higher");
   }
 
+
+  // -----
+
+  @Test
+  public void Scenario1321B() {
+
+    // Given
+    steps.iAmOnScreen("student");
+    steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
+    steps.iAmOnScreenAndPressButton("student", "outstanding");
+    steps.iAmOnScreen("grade");
+    steps.iRotateScreen("grade");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "9", "lower");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "10", "higher");
+    steps.iAmOnScreenAndPressButton("grade", "lower");
+    steps.iAmOnScreen("student");
+
+    // When
+    steps.iRotateScreen("student");
+
+    // Then
+    steps.iAmOnScreenAndSeeValueOnView("student", "9", "grade");
+  }
+
+  @Test
+  public void Scenario1422B() {
+
+    // Given
+    steps.iAmOnScreen("student");
+    steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
+    steps.iAmOnScreenAndPressButton("student", "outstanding");
+    steps.iAmOnScreen("grade");
+    steps.iRotateScreen("grade");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "9", "lower");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "10", "higher");
+    steps.iAmOnScreenAndPressButton("grade", "higher");
+    steps.iAmOnScreen("student");
+
+    // When
+    steps.iRotateScreen("student");
+
+    // Then
+    steps.iAmOnScreenAndSeeValueOnView("student", "10", "grade");
+  }
+
+  // -----
+
+
+  @Test
+  public void Scenario1531B() {
+
+    // Given
+    steps.iAmOnScreen("student");
+    steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
+    steps.iAmOnScreenAndPressButton("student", "mention");
+    steps.iAmOnScreen("grade");
+    steps.iRotateScreen("grade");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "7", "lower");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "8", "higher");
+    steps.iAmOnScreenAndPressButton("grade", "higher");
+    steps.iAmOnScreen("student");
+
+    // When
+    steps.iRotateScreen("student");
+
+    // Then
+    steps.iAmOnScreenAndSeeValueOnView("student", "8", "grade");
+  }
+
+  @Test
+  public void Scenario1632B() {
+
+    // Given
+    steps.iAmOnScreen("student");
+    steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
+    steps.iAmOnScreenAndPressButton("student", "mention");
+    steps.iAmOnScreen("grade");
+    steps.iRotateScreen("grade");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "7", "lower");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "8", "higher");
+    steps.iAmOnScreenAndPressButton("grade", "lower");
+    steps.iAmOnScreen("student");
+
+    // When
+    steps.iRotateScreen("student");
+
+    // Then
+    steps.iAmOnScreenAndSeeValueOnView("student", "7", "grade");
+  }
+
+  // -----
+
+  @Test
+  public void Scenario1741B() {
+
+    // Given
+    steps.iAmOnScreen("student");
+    steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
+    steps.iAmOnScreenAndPressButton("student", "pass");
+    steps.iAmOnScreen("grade");
+    steps.iRotateScreen("grade");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "5", "lower");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "6", "higher");
+    steps.iAmOnScreenAndPressButton("grade", "higher");
+    steps.iAmOnScreen("student");
+
+    // When
+    steps.iRotateScreen("student");
+
+    // Then
+    steps.iAmOnScreenAndSeeValueOnView("student", "6", "grade");
+  }
+
+
+  @Test
+  public void Scenario1842B() {
+
+    // Given
+    steps.iAmOnScreen("student");
+    steps.iAmOnScreenAndSeeValueOnView("student", "0", "grade");
+    steps.iAmOnScreenAndPressButton("student", "pass");
+    steps.iAmOnScreen("grade");
+    steps.iRotateScreen("grade");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "5", "lower");
+    steps.iAmOnScreenAndSeeValueOnView("grade", "6", "higher");
+    steps.iAmOnScreenAndPressButton("grade", "lower");
+    steps.iAmOnScreen("student");
+
+    // When
+    steps.iRotateScreen("student");
+
+    // Then
+    steps.iAmOnScreenAndSeeValueOnView("student", "5", "grade");
+  }
 
 }
